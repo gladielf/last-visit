e6a8d4a7186c67d42606786cb0dfd03d7b755628
Fermin conejo <gladielf@gmail.com>
2021-01-17 19:20:10 +0100

    Import roboto font-family

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,11 @@
 </template>
 
 <style lang="sass">
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap')
+
 #app
-    font-family: Avenir, Helvetica, Arial, sans-serif
+    font-family: 'Roboto', sans-serif
     -webkit-font-smoothing: antialiased
     -moz-osx-font-smoothing: grayscale
     text-align: center
