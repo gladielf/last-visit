1084a7a4095d97c5c083f351c3c1fed895edb319
Fermin conejo <gladielf@gmail.com>
2021-01-17 19:55:24 +0100

    clear Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  .home
+    lv-button.lv-button--light(text="LOGOUT", @lv-button-clicked="logout")
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'Home',
+    methods: {
+        logout () {
+            this.$router.push('Login');
+        }
+    }
+};
 </script>
