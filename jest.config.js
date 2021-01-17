cd1584392fb3f78988c32f3baccb7bd9690dbbec
Fermin conejo <gladielf@gmail.com>
2021-01-17 13:59:37 +0100

    Updated config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,48 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+    moduleFileExtensions: [
+        'js',
+        'jsx',
+        'json',
+        'vue'
+    ],
+    transform: {
+        '^.+\\.vue$': 'vue-jest',
+        '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+        '^.+\\.jsx?$': 'babel-jest'
+    },
+    transformIgnorePatterns: [
+        '/node_modules/'
+    ],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    snapshotSerializers: [
+        'jest-serializer-vue'
+    ],
+    testMatch: [
+        '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+    ],
+    testURL: 'http://localhost/',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**/src/**/*.{js,vue}',
+        '!**/src/main.js'
+    ],
+    coverageReporters: [
+        'lcov',
+        'html',
+        'text-summary'
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10
+        }
+    },
+    watchPlugins: [
+        'jest-watch-typeahead/filename',
+        'jest-watch-typeahead/testname'
+    ]
 }
