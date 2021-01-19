00a37fcf3ea43b1419cdec7e0e1e777efec331d7
Fermin conejo <gladielf@gmail.com>
2021-01-19 19:00:29 +0100

    Created lv-counter component

---
+++ src/components/lv-counter.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+    .lv-counter
+        p.lv-counter__digit {{ value }}
+        p.lv-counter__label {{ label }}
+
+</template>
+
+<script>
+export default {
+    name: 'lv-counter',
+    props: {
+        value: String,
+        label: String
+    }
+};
+</script>
+
+<style lang="sass">
+.lv-counter
+    width: 5em,
+    display: flex,
+    align-items: center,
+    flex-direction: column
+
+    &__digit,
+    &__label
+        margin: 0
+
+    &__digit
+        font-size: 2.5em,
+        line-height: 0.9em
+
+    &__label
+        margin-bottom: 0
+</style>
