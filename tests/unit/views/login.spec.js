34f7c8bc163957e9a6e05cd8e315930e22c94246
Fermin conejo <gladielf@gmail.com>
2021-01-19 14:19:12 +0100

    Added login view tests

---
+++ tests/unit/views/login.spec.js
@@ -0,0 +1,72 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Login from '@/views/Login.vue';
+import LvButton from '@/components/lv-button.vue';
+import LvInput from '@/components/lv-input.vue';
+
+// jest.mock('firebase', () => {
+//     return mocksdk;
+//   });
+
+// mocksdk.database().flush();
+
+const localVue = createLocalVue();
+
+localVue.component('lv-button', LvButton);
+localVue.component('lv-input', LvInput);
+
+describe('Login - Snapshot', () => {
+    it('Snapshot', () => {
+        const wrapper = shallowMount(Login, {
+            localVue
+        });
+        expect(wrapper.element).toMatchSnapshot();
+    });
+});
+
+describe('Login - computed', () => {
+    it('disableButton ', () => {
+        const wrapper = shallowMount(Login, {
+            localVue
+        });
+
+        wrapper.vm.email = '123456';
+        wrapper.vm.password = '123456';
+        wrapper.vm.errors = {
+            items: []
+        };
+
+        expect(wrapper.vm.disableButton).toBeFalsy();
+    });
+});
+
+describe('Login - methods', () => {
+    it.skip('login', async () => {
+        const mockRouter = {
+            push: jest.fn()
+        };
+
+        const wrapper = shallowMount(Login, {
+            localVue,
+            mocks: {
+                $router: mockRouter
+            },
+            global: {
+                firebase: {}
+            }
+        });
+
+        await wrapper.vm.login();
+
+        expect(mockRouter.push).toHaveBeenCalledWith('Home');
+    });
+
+    it('setValue', () => {
+        const wrapper = shallowMount(Login, {
+            localVue
+        });
+
+        wrapper.vm.setValue('foo@mail.com', 'email');
+
+        expect(wrapper.vm.email).toBe('foo@mail.com');
+    });
+});
