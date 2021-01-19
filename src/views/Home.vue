42c0b6224ae95bb38bf6ce1f605119ef2f2c21eb
Fermin conejo <gladielf@gmail.com>
2021-01-19 19:42:21 +0100

    Updated template and styles in Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,23 @@
 <template lang="pug">
-  .home
-    lv-button.lv-button--light(text="LOGOUT", @lv-button-clicked="logout")
+.view.home
+    h2.home__title Welcome!
+    p.home__subtitle The last time you accessed was
+    .home__counters-group
+        lv-counter.home__counter(value="00", label="days")
+        lv-counter.home__counter(value="00", label="hours")
+        lv-counter.home__counter(value="00", label="minutes")
+        lv-counter.home__counter(value="00", label="seconds")
+    lv-button.lv-button--light.home__button(text="LOGOUT", @lv-button-clicked="logout")
 </template>
 
 <script>
+import LvCounter from '@/components/lv-counter.vue';
 
 export default {
     name: 'Home',
+    components: {
+        LvCounter
+    },
     methods: {
         logout () {
             this.$router.push('Login');
@@ -14,3 +25,23 @@ export default {
     }
 };
 </script>
+
+<style lang="sass">
+.home
+    &__title
+        margin: 0
+        color: #000
+    &__subtitle
+        margin-top: 0.5em
+
+    &__counters-group
+        display: flex,
+        justify-content: space-between,
+        margin-top: 2.5em
+
+    &__counter
+        align-content: space-between
+
+    &__button
+        margin-top: 4em
+</style>
